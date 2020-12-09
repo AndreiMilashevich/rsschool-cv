89c09315399da0d8dca1586e254f763701a1d769
Andrei Milashevich <milashevichandrei@gmail.com>
2020-12-09 18:24:16 +0300

    feat: add script for answer board

--- raindrops/scripts/script.js
+++ raindrops/scripts/script.js
@@ -1,1 +1,29 @@
 import '../scss/index.scss';
+
+const answerBoard = document.querySelector('.answer_value');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const enterButton = document.querySelector('.enter');
+let numberButtons = document.querySelectorAll('.number');
+let answerValue = '';
+
+
+numberButtons.forEach(function(item, idx) {
+  item.addEventListener('click', function() {
+    console.log('click')
+    answerBoard.textContent += numberButtons[idx].textContent;
+  })
+})
+
+clearButton.addEventListener('click', function() {
+  answerBoard.textContent = '';
+})
+
+deleteButton.addEventListener('click', function() {
+  answerBoard.textContent = answerBoard.textContent.slice(0, - 1);
+})
+
+
+
+
+
