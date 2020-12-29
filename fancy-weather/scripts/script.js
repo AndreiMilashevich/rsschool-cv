47a3afb1a3c3994886814a04fe83842ab2060d9b
AndreiMilashevich <milashevichandrei@gmai.com>
2020-12-29 23:32:12 +0300

    refactor: try to refactor code

--- fancy-weather/scripts/script.js
+++ fancy-weather/scripts/script.js
@@ -1,6 +1,15 @@
 import '../scss/index.scss'
 
 //variables
+let city = ''
+let countryCode = '';
+let latitude = 23;
+let longitude = 23;
+let countryName = ''
+let lang = 'ru';
+let tempFlag = 'c';
+let weatherData = 1;
+let dataCodeRu = {};
 
 const refreshButton = document.querySelector('.button_refresh');
 const arrows = document.querySelector('.button_refresh_arrows');
@@ -20,6 +29,7 @@ const lonDOM = document.querySelector('.lon');
 const latDOM = document.querySelector('.lat');
 const map = document.querySelector('.map');
 
+
 const imageLinksLibrary = [
   'https://cdn.photosight.ru/img/4/a2d/5942981_xlarge.jpg',
   'https://russianplanes.net/images/to271000/270331.jpg',
@@ -38,6 +48,14 @@ const imageLinksLibrary = [
   'https://russianplanes.net/images/to166000/165952.jpg',
   'https://russianplanes.net/images/to142000/141759.jpg'
 ];
+const dayOfWeekEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+const dayOfWeekRu = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const dayOfWeekShortEn = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const dayOfWeekShortRu = ['Вск', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+const monthTitleEn = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthTitleRu = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
+const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${lang}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
 
 const mapOptions = {
   enableHighAccuracy: true,
@@ -45,13 +63,7 @@ const mapOptions = {
   maximumAge: 0
 };
 
-let city = ''
-let countryCode = '';
-let latitude;
-let longitude;
-let countryName = ''
-let lang;
-let tempFlag = 'c';
+
 
 //function declarations
 
@@ -62,14 +74,15 @@ async function setBackground() {
     const data = await res.json();
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
   } catch(e) {
-    console.log(e);
     const num = Math.floor(Math.random() * imageLinksLibrary.length);
     document.body.style.backgroundImage = `url(${imageLinksLibrary[num]})`;
   } 
 }
 
-function getCoordsFromNavigator(pos) {
-  const crd = pos.coords;
+
+
+function getCoordsFromNavigator(position) {
+  const crd = position.coords;
   latitude = parseFloat(crd.latitude).toPrecision(4);
   longitude = parseFloat(crd.longitude).toPrecision(4);
   getMap();   // attention
@@ -78,8 +91,27 @@ function getCoordsFromNavigator(pos) {
 
 function getCoordsFromNavigatorError(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
-  getPlace();
+  getPlaceByIp();
   getWeatherByCity();
+}
+
+async function getPlaceByIp() {
+  try {
+    const url = 'https://ipinfo.io/json?token=cb5f57499775d8'
+    const resp = await fetch(url);
+    const data = await resp.json();
+    city = data.city;
+    countryCode = data.country;
+    const loc = data.loc.split(',');
+    latitude = loc[0];
+    longitude = loc[1];
+    getMap();
+    getWeatherByCity();
+    console.log(city, countryCode, loc);
+} catch {
+    console.log('Error, do not get your IP');
+    getMap();
+  }
 }
 
 function timeInsert() {
@@ -127,8 +159,6 @@ function setContentEn() {
     getCountryByCode();
 }
 
-
-
 function setPageContent() {
   if (lang === 'en') {
     setContentEn();
@@ -154,174 +184,30 @@ function convertTemp(temp) {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-searchButton.addEventListener('click', () => {
-    searchCity();  
-});
-
-async function searchCity() {
-  city = inputField.value;
-  await getWeatherByCity(city);
-  setPageContent();
-}
-
-async function getWeather(url) {
-  const response = await fetch(url);
-  const data = respon
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function getWeatherByCoord() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${language}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
-  const res = await fetch(url);
-  const data = await res.json();
-  countryCode = await data.sys.country;
-  await getCountryByCode(countryCode);
-  setWeather(data, countryName);
-}
-
-async function getWeatherByCity() {
-  let searchSity = document.querySelector('.input_field');
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
-  const res = await fetch(url);
-  const data = await res.json();
-  latitude = data.coord.lat;
-  longitude = data.coord.lon;
-  console.log(data);
-  countryCode = data.sys.country;
-  await getCountryByCode(countryCode);
-  setWeather(data, countryName);
-  getMap();
-}
-
-
-
-function setWeather(data, countryName) {
-  
-  cityDOM.textContent = `${data.name}, ${countryName}`;
-  tempNow.textContent = `${convertTemp(data.main.temp)}°`;
-  feels.textContent = `${convertTemp(data.main.feels_like)}`;
-  wind.textContent = `${data.wind.speed}`;
-  humidity.textContent = `${data.main.humidity}`;
-  weatherDescription.textContent = `${data.weather[0].description}`;
-  switch(data.weather[0].main) {
-    case('Snow'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/snow.svg')";
-    break;
-    case('Thunderstorm'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/thunderstorms.svg')";
-    break;
-    case('Drizzle'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/drizzle.svg')";
-    break;
-    case('Fog'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/mist.svg')";
-    break;
-    case('Haze'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/mist.svg')";
-    break;
-    case('Clean'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/clear-day.svg')";
-    break;
-    case('Clouds'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/cloudy.svg')";
-    break;
-    case('Rain'):
-    weatherNowIcon.style.backgroundImage = "url('../icons/rain.svg')";
-    break;
-    default:
-    weatherNowIcon.style.backgroundImage = "url('../icons/partly-cloudy-day.svg')";
-  }
-}
-
-async function getPlace() {
-  try {
-  const url = 'https://ipinfo.io/json?token=cb5f57499775d8';
-  const res = await fetch(url);
-  const data = await res.json();
-  city = data.city;
-  countryCode = data.country;
-  let loc = data.loc.split(',');
-  latitude = loc[0];
-  longitude = loc[1];
-  getMap();
-  getWeatherByCity();
-} catch {
-  console.log('Error, do not get your IP');
-  getMap();
-  }
-}
-
-
-
-getLang();
-setPageContent();
-
-
-refreshButton.addEventListener('click',  () => {
-  arrows.classList.toggle('rotate');
-  setBackground();
-})
-langButton.addEventListener('click', switchPageContent);
-
-
-
-
-
-
-let dayOfWeekEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
-let dayOfWeekRu = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-let dayOfWeekShortEn = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-let dayOfWeekShortRu = ['Вск', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-let monthTitleEn = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let monthTitleRu = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-
-
-
 function getMap() {
-  
-  let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+  const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
   mapboxgl.accessToken = 'pk.eyJ1Ijoiam9objQ4NDIiLCJhIjoiY2tqNGUwd3dyMGp5MjJ3bnZkaGg5NnE2dyJ9.U0xQhDaQracUXFn6vxkwUw';
-  let map = new mapboxgl.Map({
+  const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   zoom: 11,
   center: [longitude, latitude]
   });
 }
+
+async function getCountryCodeDataRu() {
+  const url = `../countries.json`;
+  const responce = await fetch(url);
+  const data = await responce.json();
+  dataCodeRu = data;
+}
+
   
-async function getCountryByCode(code) {
+async function getCountryByCode() {
   if (lang === 'ru') {
-    const url = `../countries2.json`;
-    const res = await fetch(url);
-    const data = await res.json();
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].ALFA2 === code) {
-        countryName = data[i].SHORTNAME; 
+    for (let i = 0; i < dataCodeRu.length; i++) {
+      if (dataCodeRu[i].ALFA2 === countryCode) {
+        countryName = dataCodeRu[i].SHORTNAME; 
         countryName = countryName.split(' ');
         for (let i = 0; i < countryName.length; i++){
           countryName[i] = countryName[i].split('');
@@ -336,28 +222,180 @@ async function getCountryByCode(code) {
      }
     }
   } else if (lang === 'en') {
-    const url = `https://restcountries.eu/rest/v2/alpha/${code}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    countryName = data.name;
-    console.log(data.name);
-  }
-  
-  
+      const url = `https://restcountries.eu/rest/v2/alpha/${countryCode}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      countryName = data.name;   
+      console.log(data.name);
+    }
 }
 
 async function getForecast() {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${lang}&cnt=25&appid=96e6b196f9fa76950df28c29b8eaa59c`;
   const res = await fetch(url);
+  const data = await res.json();  
+}
+
+async function getWeatherByCoord(lang) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${lang}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
+  const res = await fetch(url);
   const data = await res.json();
+}
+
+
+async function getWeatherByCity() {
+  let searchSity = document.querySelector('.input_field');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
+  const res = await fetch(url);
+  const data = await res.json();
+  latitude = data.coord.lat;
+  longitude = data.coord.lon;
   console.log(data);
-  
+  countryCode = data.sys.country;
+  await getCountryByCode(countryCode);
+  setWeather(data, countryName);
+  getMap();
 }
 
 //functions call
 
+refreshButton.addEventListener('click', () => {
+  setBackground();
+  arrows.classList.toggle('rotate');
+});
 
+navigator.geolocation.getCurrentPosition(getCoordsFromNavigator, getCoordsFromNavigatorError, mapOptions);
 setBackground();
 timeInsert();
-navigator.geolocation.getCurrentPosition(getCoordsFromNavigator, getCoordsFromNavigatorError, mapOptions);
-getForecast()
+getLang();
+setPageContent();
+//getCountryByCode();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// searchButton.addEventListener('click', () => {
+//     searchCity();  
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getWeatherByCity() {
+//   let searchSity = document.querySelector('.input_field');
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&appid=96e6b196f9fa76950df28c29b8eaa59c`;
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   latitude = data.coord.lat;
+//   longitude = data.coord.lon;
+//   console.log(data);
+//   countryCode = data.sys.country;
+//   await getCountryByCode(countryCode);
+//   setWeather(data, countryName);
+//   getMap();
+// }
+
+
+
+// function setWeather(data, countryName) {
+//   cityDOM.textContent = `${data.name}, ${countryName}`;
+//   tempNow.textContent = `${convertTemp(data.main.temp)}°`;
+//   feels.textContent = `${convertTemp(data.main.feels_like)}`;
+//   wind.textContent = `${data.wind.speed}`;
+//   humidity.textContent = `${data.main.humidity}`;
+//   weatherDescription.textContent = `${data.weather[0].description}`;
+//   switch(data.weather[0].main) {
+//     case('Snow'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/snow.svg')";
+//     break;
+//     case('Thunderstorm'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/thunderstorms.svg')";
+//     break;
+//     case('Drizzle'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/drizzle.svg')";
+//     break;
+//     case('Fog'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/mist.svg')";
+//     break;
+//     case('Haze'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/mist.svg')";
+//     break;
+//     case('Clean'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/clear-day.svg')";
+//     break;
+//     case('Clouds'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/cloudy.svg')";
+//     break;
+//     case('Rain'):
+//     weatherNowIcon.style.backgroundImage = "url('../icons/rain.svg')";
+//     break;
+//     default:
+//     weatherNowIcon.style.backgroundImage = "url('../icons/partly-cloudy-day.svg')";
+//   }
+// }
+
+
+
+
+
+
+
+
+langButton.addEventListener('click', switchPageContent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
